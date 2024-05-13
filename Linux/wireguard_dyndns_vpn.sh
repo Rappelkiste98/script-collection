038a78d2d3ebe64071388c2ec98bbdacc40d83e8
Rappelkiste98 <58746798+Rappelkiste98@users.noreply.github.com>
2024-05-13 13:51:37 +0200

    Update wireguard_dyndns_vpn.sh

--- Linux/wireguard_dyndns_vpn.sh
+++ Linux/wireguard_dyndns_vpn.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
-# Config ab hier:
-interface="wgvpn0" # Name der Schnittstelle, die geprueft werden soll
-domain="wireguard.vpn.com" # Domain des Peers, dessen IP geprueft werden soll
 
+# Config ab hier:
+interface="wgvpn_test" # Name der Schnittstelle, die geprueft werden soll
+domain="test.domain.de" # Domain des Peers, dessen IP geprueft werden soll
 
 # Programmablauf ab hier:
-cip4=$(wg show $interface endpoints | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
-cip6=$(wg show $interface endpoints | grep -E -o "([0-9a-f]{1,4}[:]){1,7}[0-9a-f]{1,4}")
+wgIP4=$(wg show $interface endpoints | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
+wgIP6=$(wg show $interface endpoints | grep -E -o "([0-9a-f]{1,4}[:]){2,7}[0-9a-f]{1,4}")
+wgHandshakeDiff=$[ $(date +%s) - $(wg show $interface latest-handshakes | grep -E -o "[0-9]{10,99}") ]
+echo "wg IPv4: $wgIP4"
+echo "wg IPv6: $wgIP6"
+echo "wg Handshake Diff: $wgHandshakeDiff"
 
 digIP4=$(dig +short $domain A)
 digIP6=$(dig +short $domain AAAA)
+echo "dig IPv4: $digIP4"
+echo "dig IPv6: $digIP6"
 
-echo "IPv4 | wirguard: $cip4 dns: $digIP4\n"
-echo "IPv6 | wirguard: $cip6 dns: $digIP6\n"
-
-if [ ! -z "$cip4" -a ! -z "$digIP4" ]
-then
-        if [ "$digIP4" != "$cip4" ]
-        then
+if [ ! -z "$wgIP4" ] && [ ! -z "$digIP4" ]; then
+        if [ "$digIP4" != "$wgIP4" ]; then
                 echo "IPv4 Adressen unterscheiden sich! Interface wird neu gestartet!"
                 /usr/sbin/service wg-quick@$interface restart
                 exit 0
         fi
 fi
 
-if [ ! -z "$cip6" -a ! -z "$digIP6" ]
-then
-        if [ "$digIP6" != "$cip6" ]
-        then
+if [ ! -z "$wgIP6" ] && [ ! -z "$digIP6" ]; then
+        if [ "$digIP6" != "$wgIP6" ]; then
                  echo "IPv6 Adressen unterscheiden sich! Interface wird neu gestartet!"
                 /usr/sbin/service wg-quick@$interface restart
                 exit 0
         fi
+fi
+
+if { { [ -z "$wgIP4" ] && [ ! -z "$digIP4" ]; } || { [ -z "$wgIP6" ] && [ -n "$digIP6" ]; } } && (( $wgHandshakeDiff > 300 )); then
+        echo "Interface fehlerhaft, letzter Handshake laenger als 5min! Interface wird neu gestartet!"
+        /usr/sbin/service wg-quick@$interface restart
+        exit 0
 fi
